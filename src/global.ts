3e55aedbc6d3545021809efb2fe8768e7cbaece1
JhonSenna <jhonnyoliveira.jhon@gmail.com>
2021-04-28 23:50:04 -0300

    global style definition

---
+++ src/global.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  *{
+    border: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: 'Poppins', sans-serif;
+  }
+
+  body{
+    background-color: #E0D7EC
+  }
+  
+`;
